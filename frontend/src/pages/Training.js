d059230eeca2ef63b62f890351c4160de9f88a35
Shevelev Viacheslav <shevslawa@gmail.com>
2020-07-11 15:32:05 +0300

    Problem with categories is resolved

--- frontend/src/pages/Training.js
+++ frontend/src/pages/Training.js
@@ -2,19 +2,20 @@ import React, {Fragment} from 'react';
 import PracticeServers from './src/PracticeServers.png';
 import TacticEditor from './src/TacticEditor.png';
 import Theory from './src/Theory.png';
+import {NavLink} from 'react-router-dom'
 
 export const Training = () => {
     return (
         <Fragment>
-            <a href="/servers">
-                <div className="TrainingCategories" style={{backgroundImage: "url(" + PracticeServers + ")"}}></div>
-            </a>
-            <a href="/tacticeditor">
-                <div className="TrainingCategories" style={{backgroundImage: "url(" + TacticEditor + ")"}}></div>
-            </a>
-            <a href="/theory">
-                <div className="TrainingCategories" style={{backgroundImage: "url(" + Theory + ")"}}></div>
-            </a>
+            <NavLink exact={true} className="categories_nav__link TrainingCategories" style={{backgroundImage: "url(" + PracticeServers + ")"}} to="/servers">
+                     <plaintext className="TCtext">PracticeServers</plaintext>
+            </NavLink>
+            <NavLink exact={true} className="categories_nav__link TrainingCategories" style={{backgroundImage: "url(" + Theory + ")"}} to="/theory">
+                <plaintext className="TCtext">Theory</plaintext> 
+            </NavLink>
+            <NavLink exact={true} className="categories_nav__link TrainingCategories" style={{backgroundImage: "url(" + TacticEditor + ")"}} to="/theory">
+                <plaintext className="TCtext">TacticEditor</plaintext> 
+            </NavLink>
         </Fragment>
     )
 }
